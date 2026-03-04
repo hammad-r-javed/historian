78f7dbcfa8c2d9004e2a742465bf999c071ee02f
hammad-r-javed <128047144+hammad-r-javed@users.noreply.github.com>
2026-03-04 00:11:26 +0000

    switching to non-interactive CLI. Mapped valud arguments to corresponding functions

--- historian.rkt
+++ historian.rkt
@@ -1,26 +1,51 @@
-#lang racket/base
+#lang racket
 
-(define [main]
-  (define args (current-command-line-arguments))
-  (define args-len (vector-length args))
-
-  (unless [args-len-valid? args-len]
-    [begin
-      (printf "Invalid number of arguments\n\n")
-      (display-help-options)
-      (exit 1)])
-
-  ; (displayln [ve-ref args 0])
-  (displayln "DEBUG - good so far"))
+; FUNCTIONS
 
 (define [args-len-valid? len]
-    (= len 1))
+    (= len 2))
+
+(define [add-entry entry-string]
+  (displayln "DEBUG - add-entry called"))
+
+(define [search-entries keyword]
+  (displayln "DEBUG - search-entries called"))
+
+(define [copy-entry entry-id]
+  (displayln "DEBUG - copy-entry called"))
+
+; TODO - Need more informative error messaging
+(define [invalid-args-early-return]
+    (printf "Invalid args\n\n")
+    (display-help-options)
+    (exit 1))
 
 (define [display-help-options]
-  (displayln "\033[1mhistorian\033[22m does not take CLI args other than mode (defined blow).")
-  (displayln "Once mode is selected, further inputs can be made\n")
-  (displayln "-a  Add new entry")
-  (displayln "-s  Search entries using keyword")
-  (displayln "-c  Copy entry using ID"))
+  (displayln "\033[1mhistorian\033[22m\n")
+  (displayln "Usage - historian <command> <arg>")
+  (displayln "Valid commands and args")
+  (displayln "-a <string>  Add new entry")
+  (displayln "-s <string>  Search entries using keyword")
+  (displayln "-c <string>  Copy entry using ID obtained using search"))
+
+; DATA
+
+(define args (current-command-line-arguments))
+(define args-len (vector-length args))
+(define args-func-map
+  [hash "-a" add-entry
+        "-s" search-entries
+        "-c" copy-entry])
+
+; MAIN
+
+(define [main]
+  (unless [args-len-valid? args-len]
+    (invalid-args-early-return))
+
+  (define action [hash-ref args-func-map (vector-ref args 0) #f])
+  (match action
+    [#f (invalid-args-early-return)]
+    [_  (action [vector-ref args 1])]))
 
 (main)
